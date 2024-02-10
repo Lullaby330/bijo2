e249570ffecb311d92bbd6d51557d326fb33b30c
Lullaby330 <69coder@gmail.com>
2024-02-10 13:52:21 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,6 +11,7 @@ from config import (
     CHANNEL_ID,
     FORCE_SUB_CHANNEL,
     FORCE_SUB_GROUP,
+    FORCE_SUB_ORANG,
     LOGGER,
     OWNER,
     TG_BOT_TOKEN,
@@ -88,6 +89,29 @@ class Bot(Client):
                 )
                 self.LOGGER(__name__).warning(
                     f"Pastikan @{self.username} adalah admin di Group Tersebut, Chat ID F-Subs Group Saat Ini: {FORCE_SUB_GROUP}"
+                )
+                self.LOGGER(__name__).info(
+                    "Bot Berhenti. Gabung Group https://t.me/SharingUserbot untuk Bantuan"
+                )
+                sys.exit()
+                 if FORCE_SUB_ORANG:
+            try:
+                info = await self.get_chat(FORCE_SUB_ORANG)
+                link = info.invite_link
+                if not link:
+                    await self.export_chat_invite_link(FORCE_SUB_ORANG)
+                    link = info.invite_link
+                self.invitelink3 = link
+                self.LOGGER(__name__).info(
+                    f"FORCE_SUB_ORANG detected!\n┌ Title: {info.title}\n└ Chat ID: {info.id}\n——"
+                )
+            except Exception as a:
+                self.LOGGER(__name__).warning(a)
+                self.LOGGER(__name__).warning(
+                    "Bot tidak dapat Mengambil link invite dari FORCE_SUB_ORANG!"
+                )
+                self.LOGGER(__name__).warning(
+                    f"Pastikan @{self.username} adalah admin di Group Tersebut, Chat ID F-Subs Group Saat Ini: {FORCE_SUB_ORANG}"
                 )
                 self.LOGGER(__name__).info(
                     "Bot Berhenti. Gabung Group https://t.me/SharingUserbot untuk Bantuan"
