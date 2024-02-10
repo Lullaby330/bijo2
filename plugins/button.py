d69e8428934d5d96a8f8acc65c93567d360bbe0f
Lullaby330 <69coder@gmail.com>
2024-02-10 13:45:53 +0700

    Update button.py

--- plugins/button.py
+++ plugins/button.py
@@ -1,18 +1,18 @@
 
 
-from config import FORCE_SUB_CHANNEL, FORCE_SUB_GROUP
+from config import FORCE_SUB_CHANNEL, FORCE_SUB_GROUP, FORCE_SUB_ORANG
 from pyrogram.types import InlineKeyboardButton
 
 
 def start_button(client):
-    if not FORCE_SUB_CHANNEL and not FORCE_SUB_GROUP:
+    if not FORCE_SUB_CHANNEL and not FORCE_SUB_GROUP or not FORCE_SUB_ORANG:
         buttons = [
             [
                 InlineKeyboardButton(text="Close", callback_data="close"),
             ],
         ]
         return buttons
-    if not FORCE_SUB_CHANNEL and FORCE_SUB_GROUP:
+    if not FORCE_SUB_CHANNEL and FORCE_SUB_GROUP or FORCE_SUB_ORANG:
         buttons = [
             [
                 InlineKeyboardButton(text="Group", url=client.invitelink2),
@@ -22,7 +22,7 @@ def start_button(client):
             ],
         ]
         return buttons
-    if FORCE_SUB_CHANNEL and not FORCE_SUB_GROUP:
+    if FORCE_SUB_CHANNEL and not FORCE_SUB_GROUP or FORCE_SUB_ORANG:
         buttons = [
             [
                 InlineKeyboardButton(text="Channel", url=client.invitelink),
@@ -32,11 +32,22 @@ def start_button(client):
             ],
         ]
         return buttons
-    if FORCE_SUB_CHANNEL and FORCE_SUB_GROUP:
+    if FORCE_SUB_CHANNEL and FORCE_SUB_GROUP or not FORCE_SUB_ORANG:
+        buttons = [
+            [
+                InlineKeyboardButton(text="Channel", url=client.invitelink3),
+            ],
+            [
+                InlineKeyboardButton(text="Close", callback_data="close"),
+            ],
+        ]
+        return buttons
+    if FORCE_SUB_CHANNEL and FORCE_SUB_GROUP or FORCE_SUB_ORANG:
         buttons = [
             [
                 InlineKeyboardButton(text="Channel", url=client.invitelink),
                 InlineKeyboardButton(text="Group", url=client.invitelink2),
+                InlineKeyboardButton(text="Group", url=client.invitelink3),
             ],
             [InlineKeyboardButton(text="Close", callback_data="close")],
         ]
@@ -44,7 +55,7 @@ def start_button(client):
 
 
 def fsub_button(client, message):
-    if not FORCE_SUB_CHANNEL and FORCE_SUB_GROUP:
+    if not FORCE_SUB_CHANNEL and FORCE_SUB_GROUP or FORCE_SUB_ORANG:
         buttons = [
             [
                 InlineKeyboardButton(text="Group", url=client.invitelink2),
@@ -62,7 +73,7 @@ def fsub_button(client, message):
         except IndexError:
             pass
         return buttons
-    if FORCE_SUB_CHANNEL and not FORCE_SUB_GROUP:
+    if FORCE_SUB_CHANNEL and not FORCE_SUB_GROUP or FORCE_SUB_ORANG:
         buttons = [
             [
                 InlineKeyboardButton(text="Channel", url=client.invitelink),
@@ -80,11 +91,31 @@ def fsub_button(client, message):
         except IndexError:
             pass
         return buttons
-    if FORCE_SUB_CHANNEL and FORCE_SUB_GROUP:
+if FORCE_SUB_CHANNEL and FORCE_SUB_GROUP or not FORCE_SUB_ORANG:
+        buttons = [
+            [
+                InlineKeyboardButton(text="Channel", url=client.invitelink3),
+            ],
+        ]
+        try:
+            buttons.append(
+                [
+                    InlineKeyboardButton(
+                        text="Coba Lagi",
+                        url=f"https://t.me/{client.username}?start={message.command[1]}",
+                    )
+                ]
+            )
+        except IndexError:
+            pass
+        return buttons
+    
+    if FORCE_SUB_CHANNEL and FORCE_SUB_GROUP or FORCE_SUB_ORANG:
         buttons = [
             [
                 InlineKeyboardButton(text="Channel", url=client.invitelink),
                 InlineKeyboardButton(text="Group", url=client.invitelink2),
+                InlineKeyboardButton(text="Group", url=client.invitelink3),
             ],
         ]
         try:
